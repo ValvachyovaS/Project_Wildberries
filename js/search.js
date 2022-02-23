6a1a144532ecebe657aa4adb38428d6e7fede072
ValvachyovaS <cbetavit@gmail.com>
2022-02-24 02:40:56 +0300

    event click on input

--- js/search.js
+++ js/search.js
@@ -2,8 +2,7 @@ const search = function () {
     const inputSearch = document.querySelector('.search-block > input');
     const buttonSearch = document.querySelector('.search-block > button');
 
-    inputSearch.addEventListener('input', (event) => {
-        console.log('typing');
+    inputSearch.addEventListener('click', (event) => {
         console.log(event.target.value);
     });
 };
