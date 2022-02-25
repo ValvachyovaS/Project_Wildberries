c09c1864e612c862aa92c15809b24b78aaa4f0e3
ValvachyovaS <cbetavit@gmail.com>
2022-02-25 20:36:33 +0300

    day 3

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,26 +1,66 @@
 const getGoods = () => {
-    const links = document.querySelectorAll('.navigation-link');
+    try {
+        const renderGoods = (goods) => {
+            const goodsContainer = document.querySelector('.long-goods-list');
+            goodsContainer.innerHTML = '';
 
-    const getData = () => {
-        fetch('/db/db.json')
-            .then((res) => res.json())
-            .then((data) => localStorage.setItem('data', JSON.stringify(data)));
-    };
+            goods.forEach(good => {
+                const goodBlock = document.createElement('div');
+                goodBlock.classList.add('col-lg-3', 'col-sm-6');
+                goodBlock.innerHTML = `
+            <div class="goods-card">
+				<span class="label ${good.label ? '' : 'd-none'}">${good.label}</span>
+				<img src="db/${good.img}" alt="image: ${good.name}" class="goods-image">
+				<h3 class="goods-title">${good.name}</h3>
+				<p class="goods-description">${good.description}</p>
+				<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+					<span class="button-price">$${good.price}</span>
+                </button>
+			</div>`;
+                goodsContainer.append(goodBlock);
+            });
+        };
 
-    links.forEach((link) => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            getData();
+        const getData = (value, field, valueSearch) => {
+            fetch('/db/db.json')
+                .then((res) => res.json())
+                .then((data) => {
+                    const array = valueSearch !== null
+                        ? data.filter((good) => good.name.toLowerCase().includes(valueSearch.toLowerCase()))
+                        : field ? data.filter((item) => item[field] === value) : data;
+                    localStorage.setItem('data', JSON.stringify(array));
+                    if (window.location.pathname !== '/goods.html') {
+                        window.location.href = '/goods.html';
+                    } else {
+                        renderGoods(array);
+                    }
+
+                });
+        };
+
+        const links = document.querySelectorAll('.navigation-link');
+        links.forEach((link) => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const linkValue = link.textContent;
+                const linkDataField = link.dataset.field;
+                getData(linkValue, linkDataField, null);
+            });
         });
-    });
 
-    localStorage.setItem('goods', JSON.stringify([1, 3, 6, 9]));
-    console.log(localStorage);
+        const inputSearch = document.querySelector('.search-block > input');
+        const buttonSearch = document.querySelector('.search-block > button');
 
-    const goods = JSON.parse(localStorage.getItem('goods'));
+        buttonSearch.addEventListener('click', () => {
+            getData(null, null, inputSearch.value);
+        });
 
-    localStorage.removeItem('goods');
-    console.log(localStorage);
+        if (localStorage.getItem('data') && window.location.pathname === '/goods.html') {
+            renderGoods(JSON.parse(localStorage.getItem('data')));
+        }
+    } catch (e) {
+        console.log("Что-то пошло не так.. " + e.message);
+    }
 };
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,10 +1,1 @@
-const search = function () {
-    const inputSearch = document.querySelector('.search-block > input');
-    const buttonSearch = document.querySelector('.search-block > button');
-
-    buttonSearch.addEventListener('click', () => {
-        console.log(inputSearch.value);
-    });
-};
-
-search();
+// move to getGoods()
